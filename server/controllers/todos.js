99eccd3b5afc170989473356d3a707407791a2f5
mojo706 <eugene.omar@andela.com>
2018-02-21 14:26:28 +0300

    [ft-create-controllers-routing] add the association of a todo item to a todo and also a way to retrieve the todo with its todo item, add a method for retrieve a todo with its id

---
+++ server/controllers/todos.js
@@ -0,0 +1,42 @@
+const { Todo } = require('../models');
+const { TodoItem } = require('../models');
+
+module.exports = {
+  create(req, res) {
+    return Todo
+      .create({
+        title: req.body.title,
+      })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return Todo
+      .findAll({
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error));
+  },
+  retrieve(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return res.status(200).send(todo);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+};
