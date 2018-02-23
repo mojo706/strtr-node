f43817586b1f3c8c6fbf6bf048a8fe0f798b6937
mojo706 <eugene.omar@andela.com>
2018-02-23 12:05:27 +0300

    add method to update a todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -10,6 +10,7 @@ module.exports = {
       .then(todo => res.status(201).send(todo))
       .catch(error => res.status(400).send(error));
   },
+
   list(req, res) {
     return Todo
       .findAll({
@@ -21,6 +22,7 @@ module.exports = {
       .then(todos => res.status(200).send(todos))
       .catch(error => res.status(400).send(error));
   },
+
   retrieve(req, res) {
     return Todo
       .findById(req.params.todoId, {
@@ -36,6 +38,30 @@ module.exports = {
           });
         }
         return res.status(200).send(todo);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Todo
+      .findById(req.params.todoId, {
+        include: [{
+          model: TodoItem,
+          as: 'todoItems',
+        }],
+      })
+      .then((todo) => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo Not Found',
+          });
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title,
+          })
+          .then(() => res.status(200).send(todo)) // Send back the updated todo.
+          .catch(error => res.status(400).send(error));
       })
       .catch(error => res.status(400).send(error));
   },
